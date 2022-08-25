b9dd7807a4bee46a00b64624cdb8ee9a033962cb
AL07839 <AL07839@33.192.71.137>
2022-08-25 15:26:41 +0100

    Initial commit

---
+++ JavaHelloWorld/src/main/MessageGen.java
@@ -0,0 +1,10 @@
+package main;
+
+public class MessageGen {
+	
+	public String getMessage() {
+		String firstMessage = "Hello, World!";
+		return firstMessage;
+	}
+
+}
---
+++ JavaHelloWorld/src/main/helloWorldJava.java
@@ -0,0 +1,9 @@
+package main;
+
+public class helloWorldJava {
+
+	public static void main(String[] args) {
+		MessageGen receiveMessage = new MessageGen();
+		System.out.println(receiveMessage.getMessage());
+	}
+}
